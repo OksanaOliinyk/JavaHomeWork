81dcf71cce674f013c2142b5f842cba33598ec98
Oliinyk Pomaza <oksana.olejnik@privatbank.ua>
2021-11-06 22:05:33 +0300

    init commit

---
+++ src/com/pb/oliinykpomaza/hw6/Veterinarian.java
@@ -0,0 +1,9 @@
+package com.pb.oliinykpomaza.hw6;
+
+public class Veterinarian {
+
+    public void treatAnimal(Animal animals)
+    Animal[] animals = new Animal [2]; {
+        System.out.println(animals);
+    }
+}
